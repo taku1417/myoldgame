7057c187341eb7092ef757621afac99c7ad0000f
taku1417 <58448175+taku1417@users.noreply.github.com>
2021-10-01 15:09:57 +0900

    pluginが動くように。

---
+++ myoldgame/src/module-info.java
@@ -0,0 +1,4 @@
+module myoldgame {
+	requires spigot;
+
+}
---
+++ myoldgame/src/myoldgame/Myoldgame.java
@@ -0,0 +1,19 @@
+package myoldgame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Myoldgame extends JavaPlugin {
+
+	@Override
+	public void onDisable() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onDisable();
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onEnable();
+	}
+
+}
